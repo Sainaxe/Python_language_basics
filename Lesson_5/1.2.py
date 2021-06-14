089a5f6834fad8a69bc4ad2bec50ba6dc2e3b36b
Denis <axel-crusader@mail.ru>
2021-06-14 20:02:20 +0300

    HW_5

--- Lesson_5/1.2.py
+++ Lesson_5/1.2.py
@@ -3,3 +3,25 @@ task2
 Создать текстовый файл (не программно), сохранить в нем несколько строк, выполнить подсчет количества строк,
 количества слов в каждой строке.
 '''
+
+file_2 = open('task2.txt', 'r')
+content = file_2.read()
+print(f'File contents: \n {content}')
+file_2.close()
+
+file_2 = open('task2.txt', 'r')
+content = file_2.readlines()
+for i in range(len(content)):
+    print(f'Number of characters in the file {i + 1}-th line is {len(content[i])};')
+file_2.close()
+
+file_2 = open('task2.txt', 'r')
+content = file_2.readlines()
+print(f'Number of lines in the file: {len(content)}')
+file_2.close()
+
+file_2 = open('task2.txt', 'r')
+content = file_2.read()
+content = content.split()
+print(f'The number of words in the file: {len(content)}')
+file_2.close()
--- Lesson_5/1.3.py
+++ Lesson_5/1.3.py
@@ -4,3 +4,14 @@ task3
 Определить, кто из сотрудников имеет оклад менее 20 тыс., вывести фамилии этих сотрудников.
 Выполнить подсчет средней величины дохода сотрудников.
 '''
+
+with open('task3.txt', 'r') as file_3:
+    salary = []
+    less_than_20k_salary = []
+    list_3 = file_3.read().split('\n')
+    for i in list_3:
+        i = i.split()
+        if int(i[1]) < 20000:
+           less_than_20k_salary.append(i[0])
+        salary.append(i[1])
+print(f'Salary less than 20,000: {less_than_20k_salary}, average salary {sum(map(int, salary)) / len(salary)} rub.')
